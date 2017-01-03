a6a3a370177efb3272c99c53079f6266d1051394
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-01-03 11:25:24 +0200

    [OOP Second HW] P11.VersionAttribute with the correct name.

---
+++ Homeworks/01. OOP/02. DefiningClasses Part 2/P11. VersionAttribute/VersionAttribute.cs
@@ -0,0 +1,30 @@
+﻿namespace P11.VersionAttribute
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Struct |
+                    AttributeTargets.Class |
+                    AttributeTargets.Interface |
+                    AttributeTargets.Enum |
+                    AttributeTargets.Method)]
+    public class VersionAttribute : Attribute
+    {
+        private int major;
+        private int minor;
+
+        public VersionAttribute(int major, int minor)
+        {
+            this.Major = major;
+            this.Minor = minor;
+        }
+
+        public int Major { get; private set; }
+
+        public int Minor { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Version {0}.{1}", this.Major, this.Minor);
+        }
+    }
+}
