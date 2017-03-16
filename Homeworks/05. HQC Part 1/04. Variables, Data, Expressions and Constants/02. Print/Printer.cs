4112d4e0cabd1dc73e846d4fab1eb70ab9147aab
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-03-16 13:15:36 +0200

    [HQC Part 1 HW] Variables, data expressions and constants. All tasks solved!

---
+++ Homeworks/05. HQC Part 1/04. Variables, Data, Expressions and Constants/02. Print/Printer.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.Print
+{
+    using System;
+
+    public static class Printer
+    {
+        public static void PrintStatistics(double[] statistics)
+        {
+            double maxValue = double.MinValue;
+            double minValue = double.MaxValue;
+
+            double sum = 0;
+
+            int statisticLenght = statistics.Length;
+
+            for (int i = 0; i < statisticLenght; i++)
+            {
+                double currentValue = statistics[i];
+
+                if (maxValue < currentValue)
+                {
+                    maxValue = currentValue;
+                }
+
+                if (minValue > currentValue)
+                {
+                    minValue = currentValue;
+                }
+
+                sum += currentValue;
+            }
+
+            PrintNumber(maxValue);
+            PrintNumber(minValue);
+
+            double averageValue = sum / statisticLenght;
+
+            PrintNumber(averageValue);
+        }
+
+        public static void PrintNumber(double number)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
