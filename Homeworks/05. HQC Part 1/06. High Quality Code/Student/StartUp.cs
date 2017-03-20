f15fffa5393f8de850855285913f50fb3311cbec
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-03-20 10:50:31 +0200

    [HQC Part 1 HW] Hight Quality Methods homework solved!

---
+++ Homeworks/05. HQC Part 1/06. High Quality Code/Student/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentMethod
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            Student ivan = new Student("Ivan", "Ivanov", new DateTime(2008, 04, 14), "Sofia");
+            Console.WriteLine(ivan);
+        }
+    }
+}
