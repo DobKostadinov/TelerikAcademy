99a61c9a294855e67174517292ee5e5f532a2425
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-03-21 12:43:05 +0200

    [JS UI and Dom HW] Handlebars homework - last task solved!

--- Homeworks/06. JS UI & DOM/05. Handlebars/tasks/task-3.js
+++ Homeworks/06. JS UI & DOM/05. Handlebars/tasks/task-3.js
@@ -1,9 +1,19 @@
-function solve(){
-  return function(){
-    $.fn.listview = function(data){
-      //TODO
+function solve() {
+    return function () {
+        $.fn.listview = function (data) {
+            var $this = $(this),
+                hbTemplate = handlebars.compile($(
+                    '#' +
+                    $this.attr('data-template'))
+                    .html()
+                );
+
+            for (var element of data) {
+                $this.append(hbTemplate(element));
+            }
+        };
     };
-  };
 }
 
 module.exports = solve;
+
